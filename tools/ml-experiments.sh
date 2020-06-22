80b00177d4ff4cda833cd3a95c87c4078f21232f
Jeanderson Candido <jeandersonbc@gmail.com>
2020-06-22 20:52:22 +0200

    Added scripts to run try-catch experiments

--- tools/ml-experiments.sh
+++ tools/ml-experiments.sh
@@ -36,4 +36,32 @@ run_standard_experiments() {
   done
 }
 
+
+run_trycatch_experiments() {
+  local output_base="$BASEDIR/out/ml/$PROJECT_NAME"
+
+  for model in "dt" "rf" "et" "ab" "lr"; do
+
+    local output_dir="$output_base/$model-trycatch"
+    rm -rf "$output_dir" && mkdir -p "$output_dir"
+
+    echo $PROJECT_NAME: Running model $model
+    pushd "$output_dir" || exit 1
+    time python3 -m logpred_method "$model" "$DATASET_CSV" --drops tryCatchQty_method tryCatchQty_class | tee logpred-method.log
+    popd || exit 1
+
+    for balancing in "smote" "rus"; do
+      output_dir="$output_base/$model-trycatch-$balancing"
+
+      rm -rf "$output_dir" && mkdir -p "$output_dir"
+
+      echo $PROJECT_NAME: Running balancing option $balancing
+      pushd "$output_dir" || exit 1
+      time python3 -m logpred_method "$model" "$DATASET_CSV" --balancing "$balancing" --drops tryCatchQty_method tryCatchQty_class | tee logpred-method.log
+      popd || exit 1
+    done
+  done
+}
+
 time run_standard_experiments
+time run_trycatch_experiments
