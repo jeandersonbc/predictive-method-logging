f0f60467cb5c1ad649d433345329264971ffccf6
Jeanderson Candido <jeandersonbc@gmail.com>
2020-07-13 15:41:03 +0200

    Updated paper-evaluation script

--- paper-evaluation.py
+++ paper-evaluation.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# Executable script exported and adapted from Jupyter notebook.
 
 
 import os
 import shutil
 
-import pandas as pd
 import logpred_method as experiment
-
+import pandas as pd
 from sklearn.model_selection import train_test_split
-
 
 # Use "FRACTION = None" for full dataset
 FRACTION: float = None
@@ -23,19 +21,15 @@ FRACTION: float = None
 # et: Extra Trees
 MODELS = ["lr", "ab", "rf", "dt", "et"]
 
-
 # You can ignore features on the experiment
 IGNORED_FEATURES = ["tryCatchQty_class", "tryCatchQty_method"]
 
+# Hyper-parameter tuning
+TUNING_ENABLED = True
 
-# Stores estimators and feature importances across experiments
+# Stores estimators and feature importance across experiments
 ESTIMATORS = {}
 FEATURE_IMPORTANCES = {}
-
-
-# # Utilities
-
-# In[2]:
 
 
 def merge_scores(scores):
@@ -47,17 +41,12 @@ def merge_scores(scores):
         in  - [{"a": 1, "b": 2}, {"a": 10, "b": 20}]
         out - {"a": [1, 10], "b": [2, 20]}
     """
-    merged = {k:[] for k in scores[0].keys()}
+    merged = {k: [] for k in scores[0].keys()}
     for score in scores:
         for k, v in score.items():
             merged[k].append(v)
 
     return merged
-
-
-# # Experiment CSV and Output directory
-
-# In[3]:
 
 
 csv_path = os.path.abspath(os.path.join("out", "dataset", "adyen-main", "dataset_full.csv"))
@@ -67,15 +56,10 @@ X_adyen_train, X_adyen_test, y_adyen_train, y_adyen_test = train_test_split(
     X_adyen, y_adyen, test_size=0.2, stratify=y_adyen, random_state=experiment.RANDOM_SEED
 )
 
-output_dir = os.path.abspath(os.path.join("out", "ml", "evaluation"))
+output_dir = os.path.abspath(os.path.join("out", "ml", f"evaluation-tuning-{TUNING_ENABLED}"))
 if os.path.exists(output_dir):
     shutil.rmtree(output_dir)
 os.makedirs(output_dir)
-
-
-# # RQ 1. What  is  the  performance  of  machine  learning  models in  predicting  log  placement  in  a  large-scale  enterprise system?
-
-# In[4]:
 
 
 def rq1():
@@ -87,36 +71,27 @@ def rq1():
             X_test=X_adyen_test,
             y_train=y_adyen_train,
             y_test=y_adyen_test,
-            output_to=os.path.join(output_dir, f"rq1-{model}.log")
+            output_to=os.path.join(output_dir, f"rq1-{model}.log"),
+            tuning_enabled=TUNING_ENABLED
         )
         estimator, score, fi = out
         scores.append(score)
-        
+
         # Save to the global state this run
         ESTIMATORS[model] = estimator
         FEATURE_IMPORTANCES[model] = fi
 
     return scores
 
+
 rq1_scores = rq1()
-
-
-# ## Results
-
-# In[5]:
-
 
 results_rq1 = pd.DataFrame.from_dict(merge_scores(rq1_scores)).set_index(["model"])
 results_rq1.reset_index().to_csv(
     os.path.join(output_dir, "rq1-results.csv"),
     index=False,
 )
-results_rq1["prec recall acc tn fp fn tp total".split(" ")]
-
-
-# # RQ 2. What is the impact of different class balancing strategies on prediction?
-
-# In[6]:
+print(results_rq1["prec recall acc tn fp fn tp total".split(" ")])
 
 
 # Similar to rq1 but we include sampling in the experiment now.
@@ -131,11 +106,12 @@ def rq2():
                 y_train=y_adyen_train,
                 y_test=y_adyen_test,
                 balancing=balancing,
-                output_to=os.path.join(output_dir, f"rq2-{model}-{balancing}.log")
+                output_to=os.path.join(output_dir, f"rq2-{model}-{balancing}.log"),
+                tuning_enabled=TUNING_ENABLED
             )
             estimator, score, fi = out
             scores.append(score)
-            
+
             # Save to the global state this run
             key = f"{model}-{balancing}"
             ESTIMATORS[key] = estimator
@@ -143,13 +119,8 @@ def rq2():
 
     return scores
 
+
 rq2_scores = rq2()
-
-
-# ## Results
-
-# In[7]:
-
 
 results_rq2 = pd.DataFrame.from_dict(merge_scores(rq2_scores)).set_index(["model", "balancing"])
 results_rq2.reset_index().to_csv(
@@ -158,25 +129,14 @@ results_rq2.reset_index().to_csv(
 )
 
 relevant_cols = "prec recall acc".split(" ")
-results_rq2[relevant_cols]
-
-
-# Comparative result to the baseline (no balancing). Positive value indicates improvement.
-
-# In[8]:
-
+print(results_rq2[relevant_cols])
 
 results_rq2_rel = results_rq2.loc[MODELS, relevant_cols] - results_rq1.loc[MODELS, relevant_cols]
 results_rq2_rel.reset_index().to_csv(
     os.path.join(output_dir, "rq2-results-relative.csv"),
     index=False
 )
-results_rq2_rel
-
-
-# # RQ 3. How  do  machine  learning  models  perceive  predictors?
-
-# In[9]:
+print(results_rq2_rel)
 
 
 def rank_to_df(rank):
@@ -202,21 +162,12 @@ def feature_importance_rank(selected_models, top_n=3):
     return rank
 
 
-# ## Results
-
-# In[10]:
-
-
 fi = rank_to_df(feature_importance_rank(MODELS))
 fi.to_csv(
     os.path.join(output_dir, "rq3-fi-regular.csv"),
     index=False
 )
-fi
-
-
-# In[11]:
-
+print(fi)
 
 fi_smote = rank_to_df(
     feature_importance_rank([
@@ -229,11 +180,7 @@ fi_smote.to_csv(
     os.path.join(output_dir, "rq3-fi-smote.csv"),
     index=False
 )
-fi_smote
-
-
-# In[12]:
-
+print(fi_smote)
 
 fi_rus = rank_to_df(
     feature_importance_rank([
@@ -246,13 +193,7 @@ fi_rus.to_csv(
     os.path.join(output_dir, "rq3-fi-rus.csv"),
     index=False
 )
-fi_rus
-
-
-# # RQ 4. How  well  a  model  trained  with  open-source  data  can generalize  to  the  context  of  a  large-scale  enterprise system?
-
-# In[13]:
-
+print(fi_rus)
 
 from typing import List
 
@@ -288,18 +229,8 @@ APACHE_PROJECTS = {
 
 assert len(APACHE_PROJECTS) == 29
 
-
-# In[14]:
-
-
 for k, v in APACHE_PROJECTS.items():
     print(f"{k:20} {str(v[0].shape):>15}")
-
-
-# ## Learning from all Apache projects
-
-# In[15]:
-
 
 X_apache_all = pd.concat(
     [X_apache for X_apache, _ in APACHE_PROJECTS.values()],
@@ -314,10 +245,7 @@ y_apache_all = pd.concat(
 assert sum([X.shape[0] for X, _ in APACHE_PROJECTS.values()]) == X_apache_all.shape[0]
 
 # apache dataset size, all together
-X_apache_all.shape
-
-
-# In[ ]:
+print(X_apache_all.shape)
 
 
 def rq4():
@@ -330,7 +258,7 @@ def rq4():
         y_train=y_apache_all,
         y_test=y_adyen_test,
         output_to=os.path.join(output_dir, f"rq4-{model}-apache-all.log"),
-        tuning_enabled=True
+        tuning_enabled=TUNING_ENABLED
     )
     estimator, score, fi = out
     score["project"] = "apache-all"
@@ -348,11 +276,6 @@ def rq4():
 rq4_scores_all = rq4()
 
 
-# ## Learning from Projects Individually
-
-# In[ ]:
-
-
 def rq4_individual():
     scores = []
     model = "rf"
@@ -364,7 +287,7 @@ def rq4_individual():
             y_train=Xy[1].drop(columns=["type"]),
             y_test=y_adyen_test.drop(columns=["type"]),
             output_to=os.path.join(output_dir, f"rq4-{model}-{project}.log"),
-            tuning_enabled=True
+            tuning_enabled=TUNING_ENABLED
         )
         estimator, score, fi = out
         score["project"] = project
@@ -381,12 +304,6 @@ def rq4_individual():
 
 rq4_scores_individual = rq4_individual()
 
-
-# ## Results
-
-# In[ ]:
-
-
 results_rq4 = pd.DataFrame.from_dict(
     merge_scores(
         rq4_scores_all + rq4_scores_individual
@@ -397,4 +314,3 @@ results_rq4.to_csv(
     index=False
 )
 results_rq4.drop(columns=["model", "balancing"]).sort_values(by="prec recall acc".split(" "), ascending=False)
-
