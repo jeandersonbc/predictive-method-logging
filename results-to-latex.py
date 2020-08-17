80b6e5275ee8e930c3eaa82d4ee2cbd01317d0ae
Jeanderson Candido <jeandersonbc@gmail.com>
2020-08-17 14:30:55 +0200

    Minor fixes and updates on this utility script

--- results-to-latex.py
+++ results-to-latex.py
@@ -1,4 +1,5 @@
-from os.path import abspath, join
+from sys import argv
+from os.path import join
 
 import pandas as pd
 
@@ -7,27 +8,58 @@ def int_formatter(num):
     return f"{num:,d}"
 
 
-data_dir = abspath(join("out", "ml", "evaluation-tuning-True"))
+# Path to the directory containing CSV files for rq1, rq2, rq3, and rq4
+data_dir = argv[1]
 
-cols = "prec,recall,acc,tn,fp,fn,tp,total".split(",")
+cols = "acc,prec,recall,tn,fp,fn,tp,total".split(",")
 
-rq1 = pd.read_csv(join(data_dir, "rq1-results.csv")).set_index("model")[cols]
-print(rq1.to_latex(float_format="%.2f"))
-print((rq1["prec recall acc".split(" ")].mean() * 100).to_latex(float_format="%.1f"))
-print((rq1["prec recall acc".split(" ")].std() * 100).to_latex(float_format="%.1f"))
+rq1 = (
+    pd.read_csv(join(data_dir, "rq1-results.csv"))
+    .set_index("model")[cols]
+    .sort_values(by=["acc", "prec", "recall"], ascending=False)
+)
+print("RQ1")
+print(
+    rq1.to_latex(
+        float_format="%.2f",
+        formatters={col: int_formatter for col in "tn,fp,fn,tp".split(",")},
+    )
+)
+print(
+    (
+        rq1["prec recall acc".split(" ")]
+        .agg(["mean", "std"])
+        * 100
+    ).to_latex(float_format="%.1f")
+)
 
+print("RQ2")
 rq2 = pd.read_csv(join(data_dir, "rq2-results-relative.csv")).set_index(
     ["model", "balancing"]
-)["prec recall acc".split(" ")]
+)["acc prec recall".split(" ")]
 print(rq2.to_latex(float_format="%+.2f"))
+print(
+    (
+        rq2.reset_index()
+        .drop(columns=["model"])
+        .groupby("balancing")
+        .agg(["mean", "std"])
+        * 100
+    ).to_latex(float_format="%.1f")
+)
 
+print("RQ3")
+rq3 = pd.read_csv(join(data_dir, "rq3-results.csv"))
+print(rq3[rq3["total"] > 1].to_latex(index=False))
+
+print("RQ4")
 rq4 = (
-    pd.read_csv(join(data_dir, "rq4.csv"))
-    .sort_values(by="training_size", ascending=False)
+    pd.read_csv(join(data_dir, "rq4-results.csv"))
+    .sort_values(by=["acc", "prec", "recall"], ascending=False)
     .set_index(["project"])[["training_size"] + cols]
 )
 print(
-    rq4.drop(columns="fp fn tn tp total".split(" ")).to_latex(
+    rq4.drop(columns="total tp tn fp fn".split(" ")).to_latex(
         float_format="%.2f", formatters={"training_size": int_formatter}
     )
 )
