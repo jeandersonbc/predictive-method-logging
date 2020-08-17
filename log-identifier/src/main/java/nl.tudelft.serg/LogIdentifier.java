01bf41e9a2d6942be5d37c5739dcca91b88517af
Jeanderson Candido <jeandersonbc@gmail.com>
2020-08-17 15:48:46 +0200

    Removed commented code

--- log-identifier/src/main/java/nl.tudelft.serg/LogIdentifier.java
+++ log-identifier/src/main/java/nl.tudelft.serg/LogIdentifier.java
@@ -2,18 +2,6 @@ package nl.tudelft.serg;
 
 public class LogIdentifier {
 
-    //    static {
-//        logMethods.add("info");
-//        logMethods.add("debug");
-//        logMethods.add("warn");
-//        logMethods.add("error");
-//        logMethods.add("fatal");
-//        logMethods.add("trace");
-//
-//        logClasses.add("log");
-//        logClasses.add("logger");
-//    }
-
     static public boolean isLogStatement(String statement) {
         String line = statement.toLowerCase();
         return (line.matches(".*\\.(info|warn|debug|error)\\(.*")
